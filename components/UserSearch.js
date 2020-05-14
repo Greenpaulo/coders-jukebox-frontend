3aa1ec62ee12de726d6ef1ba12dab0f487894af5
Paul Bingham <greenpaulo@yahoo.co.uk>
2020-05-14 13:25:35 +0100

    Added user search logic

--- components/UserSearch.js
+++ components/UserSearch.js
@@ -1,14 +1,38 @@
+import { useState, useContext } from 'react';
+import Link from 'next/Link';
 import UserSearchBar from './UserSearchBar';
+import { GlobalContext } from '../context/GlobalContext';
 
 
 const UserSearch = () => {
+  
+  const [filteredUsers, setFilteredUsers] = useState([])
+  
+  const { fetchProfileUser } = useContext(GlobalContext);
+  
+  const userClickHandler = (id) => {
+
+    fetchProfileUser(id, false);
+
+    // Hide dropdown
+    // document.getElementById('dropdown').style.display = "none";
+  }
+
   return (
     <div id="user-search">
-      <UserSearchBar placeholder='Search for user' searchMode='user' />
+      <UserSearchBar placeholder='Search for user' searchMode='user' setFilteredUsers={setFilteredUsers}/>
       <div id="dropdown">
         <div id="search-results">
           <ul>
-            <li>Search Results</li>
+            
+            {filteredUsers.map(user => (
+              <li key={user.id}>
+                <Link href="/profile/[userId]" as={`/profile/${user.id}`}>
+                  <a onClick={userClickHandler.bind(this, user.id)}>{user.name}</a>
+                </Link>
+              </li>
+            ))}
+
           </ul>
         </div>
       </div>
--- components/UserSearchBar.js
+++ components/UserSearchBar.js
@@ -1,13 +1,12 @@
 import { useState, useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalContext';
 
-const UserSearchbar = ({ placeholder, submitHandler }) => {
+const UserSearchbar = ({ placeholder, submitHandler, setFilteredUsers}) => {
 
   const { allUsers } = useContext(GlobalContext);
 
   const [searchInput, setSearchInput] = useState('');
 
-  const [filteredUsers, setFilteredUsers] = useState([])
 
   useEffect(() => {
     // action on update of searchInput
@@ -16,8 +15,6 @@ const UserSearchbar = ({ placeholder, submitHandler }) => {
       })
 
       setFilteredUsers(users)
-    
-
   }, [searchInput]);
 
   const onChangeHandler = (e) => {
@@ -33,7 +30,7 @@ const UserSearchbar = ({ placeholder, submitHandler }) => {
 
   return (
     <div id="main-searchbar">
-      <form onSubmit={onFormSubmit}>
+      <form onSubmit={onFormSubmit} autoComplete="off">
         <label htmlFor="search-input">
           <svg id="search-icon" height="24" viewBox="0 0 24 24" width="24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
         </label>
